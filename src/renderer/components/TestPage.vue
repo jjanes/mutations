6026b2b983391487c08d33b40cc1b3a86615602d
john janes <john@johnjanes.com>
2020-03-04 02:14:09 -0600

    init commit

---
+++ src/renderer/components/TestPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        test
+
+        <router-link to="/">Go to Foo</router-link>
+
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'TestPage'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuetify from 'src/renderer/plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
